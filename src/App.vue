f95455f0ca5bf5d5a706d84cffbba82071c8504a
Austreberto Gomez <austrebertog@gmail.com>
2019-11-25 12:17:42 -0600

    adding final touches

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <router-view/>
+    
+      <br>
+      <p class="center teal-text">Developed by: Aus 😎</p>
   </div>
 </template>
 
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="chat container">
-      <h2 class="center teal-text">Ninja Chat</h2>
+      <h2 class="center teal-text">ET Chat</h2>
+      <p class="center teal-text">Random people chatting</p>
       <div class="card">
           <div class="card-content">
               <ul class="messages" v-chat-scroll>
-                  <li v-for="message in messages" :key="message.id">
-                      <span class="teal-text">{{ message.name }}</span>
-                      <span class="grey-text text-darken-3">{{ message.content }}</span>
+                  <li v-for="message in messages" :key="message.id" class="message"> 
+                      <span class="teal-text name">{{ message.name }}</span>
+                      <br>
+                      <span class="grey-text text-darken-3 content">{{ message.content }}</span>
                       <span class="grey-text time">{{ message.timestamp }}</span>
                   </li>
               </ul>
@@ -57,10 +59,13 @@ export default {
 <style>
 .chat h2{
     font-size: 2.6em;
-    margin: 40px;
+    margin: 30px;
 }
 .chat span{
-    font-size: 1.4em;
+    
+}
+.chat .name{
+font-size: 0.8em;
 }
 .chat .time{
     display: block;
@@ -70,7 +75,14 @@ export default {
     max-height: 300px;
     overflow: auto;
 }
-
+.message {
+    background-color: #e5e2e2;
+    padding: 5px;
+    margin: 10px;
+}
+.content{
+    font-size: 1.3em;
+}
 .messages::-webkit-scrollbar-thumb{
     background-color: #aaa;
 }
